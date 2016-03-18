48caa4af08848714293264d84842dfffbf085da4
Ibrahim Abdul Rahim <mrwaim@gmail.com>
2016-03-18 12:03:42 +0800

    use individual route entries

--- routes/routes.php
+++ routes/routes.php
@@ -2,9 +2,10 @@
 
 Route::group(['middleware' => ['auth']], function () {
     Route::group(['middleware' => ['auth.admin']], function () {
-
-        Route::controllers([
-            'report' => '\Klsandbox\ReportRoute\Http\Controllers\ReportController',
-        ]);
+        Route::group(['prefix' => 'report'], function () {
+            Route::post('update-monthly-report', '\Klsandbox\ReportRoute\Http\Controllers\ReportController@postUpdateMonthlyReport');
+            Route::get('monthly-report/{month}/{year}/{filter}', '\Klsandbox\ReportRoute\Http\Controllers\ReportController@getMonthlyReport');
+            Route::get('monthly-report-list', '\Klsandbox\ReportRoute\Http\Controllers\ReportController@getMonthlyReportList');
+        });
     });
 });
