e04a2159ba20aaadc592f631f549a3d9aa5c2863
Ibrahim Abdul Rahim <mrwaim@gmail.com>
2016-03-18 12:04:24 +0800

    forgot month and year

--- routes/routes.php
+++ routes/routes.php
@@ -3,7 +3,7 @@
 Route::group(['middleware' => ['auth']], function () {
     Route::group(['middleware' => ['auth.admin']], function () {
         Route::group(['prefix' => 'report'], function () {
-            Route::post('update-monthly-report', '\Klsandbox\ReportRoute\Http\Controllers\ReportController@postUpdateMonthlyReport');
+            Route::post('update-monthly-report/{month}/{year}', '\Klsandbox\ReportRoute\Http\Controllers\ReportController@postUpdateMonthlyReport');
             Route::get('monthly-report/{month}/{year}/{filter}', '\Klsandbox\ReportRoute\Http\Controllers\ReportController@getMonthlyReport');
             Route::get('monthly-report-list', '\Klsandbox\ReportRoute\Http\Controllers\ReportController@getMonthlyReportList');
         });
