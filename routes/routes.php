40d0aafd944e6feb383da359cc31d6b2fe3385e4
Ibrahim Abdul Rahim <mrwaim@gmail.com>
2016-03-30 12:29:19 +0800

    route middleware

--- routes/routes.php
+++ routes/routes.php
@@ -1,7 +1,7 @@
 <?php
 
 Route::group(['middleware' => ['auth']], function () {
-    Route::group(['middleware' => ['auth.admin']], function () {
+    Route::group(['middleware' => ['role:admin']], function () {
         Route::group(['prefix' => 'report'], function () {
             Route::post('update-monthly-report/{month}/{year}', '\Klsandbox\ReportRoute\Http\Controllers\ReportController@postUpdateMonthlyReport');
             Route::get('monthly-report/{month}/{year}/{filter}', '\Klsandbox\ReportRoute\Http\Controllers\ReportController@getMonthlyReport');
