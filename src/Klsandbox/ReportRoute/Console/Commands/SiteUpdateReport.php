d4b6f09cc80c46729786815c013340eb8ccfffcb
Klsandbox <ibrahim@klsandbox.com>
2016-09-26 10:54:42 -0700

    update-update-report

--- src/Klsandbox/ReportRoute/Console/Commands/SiteUpdateReport.php
+++ src/Klsandbox/ReportRoute/Console/Commands/SiteUpdateReport.php
@@ -2,6 +2,7 @@
 
 namespace Klsandbox\ReportRoute\Console\Commands;
 
+use App\Models\Organization;
 use App\Models\PaymentsApprovals;
 use Klsandbox\ReportRoute\Models\MonthlyReport;
 use Klsandbox\ReportRoute\Models\MonthlyUserReport;
@@ -15,7 +16,7 @@ class SiteUpdateReport extends Command
     /**
      * @var ReportService $reportService
      */
-    protected $reportService;
+    public $reportService;
 
     public function __construct(ReportService $reportService)
     {
@@ -78,9 +79,6 @@ class SiteUpdateReport extends Command
 
         \DB::transaction(function () use ($report, $year, $month) {
 
-            $is_hq = true;
-            $organization_id = null;
-
             $userReports = MonthlyUserReport::where('monthly_report_id', '=', $report->id)
                 ->get();
 
@@ -98,38 +96,55 @@ class SiteUpdateReport extends Command
                 $paymentApprovalReport->save();
             }
 
-            $data = $this->reportService->getMonthlyReport($year, $month, $is_hq, $organization_id);
-            $report = $report->fill([
-                'year' => $data->year,
-                'month' => $data->month,
-                'orders_count' => $data->totalOrders,
-                'approved_orders_count' => $data->totalApprovedOrders,
-                'new_users_count' => $data->newUsersCount,
-                'total_users_count' => $data->totalUsersCount,
-                'total_revenue' => $data->totalRevenue,
-                'bonus_payout_cash' => $data->bonusPayoutForMonth->cash,
-                'bonus_payout_gold' => $data->bonusPayoutForMonth->gold,
-                'bonus_payout_not_chosen' => $data->bonusPayoutForMonth->bonusNotChosen,
-                'is_hq' => $is_hq,
-                'organization_id' => $organization_id,
-            ]);
-
-            $report->save();
-
-            $this->comment("Report updated id:$report->id");
-            foreach ($data->userData as $userData) {
-                $userReport = MonthlyUserReport::create([
-                    'monthly_report_id' => $report->id,
-                    'user_id' => $userData->user->id,
-                    'orders_count' => $userData->totalApprovedOrders,
-                    'introductions_count' => $userData->totalIntroductions,
-                    'total_stockist_count' => $userData->totalStockists,
-                    'bonus_payout_cash' => $userData->bonusPayoutForMonth->cash,
-                    'bonus_payout_gold' => $userData->bonusPayoutForMonth->gold,
-                    'bonus_payout_not_chosen' => $userData->bonusPayoutForMonth->bonusNotChosen,
-                    'online_payer' => (bool) $userData->onlinePayer,
+            /**
+             * @param $this
+             * @param $year
+             * @param $month
+             * @param $is_hq
+             * @param $organization_id
+             * @param $report
+             */
+            function generateReport($self, $year, $month, $is_hq, $organization_id, $report)
+            {
+                $data = $self->reportService->getMonthlyReport($year, $month, $is_hq, $organization_id);
+                $report = $report->fill([
+                    'year' => $data->year,
+                    'month' => $data->month,
+                    'orders_count' => $data->totalOrders,
+                    'approved_orders_count' => $data->totalApprovedOrders,
+                    'new_users_count' => $data->newUsersCount,
+                    'total_users_count' => $data->totalUsersCount,
+                    'total_revenue' => $data->totalRevenue,
+                    'bonus_payout_cash' => $data->bonusPayoutForMonth->cash,
+                    'bonus_payout_gold' => $data->bonusPayoutForMonth->gold,
+                    'bonus_payout_not_chosen' => $data->bonusPayoutForMonth->bonusNotChosen,
+                    'is_hq' => $is_hq,
+                    'organization_id' => $organization_id,
                 ]);
-                $this->comment("  User Report created id:$userReport->id");
+
+                $report->save();
+
+                $self->comment("Report updated id:$report->id");
+                foreach ($data->userData as $userData) {
+                    $userReport = MonthlyUserReport::create([
+                        'monthly_report_id' => $report->id,
+                        'user_id' => $userData->user->id,
+                        'orders_count' => $userData->totalApprovedOrders,
+                        'introductions_count' => $userData->totalIntroductions,
+                        'total_stockist_count' => $userData->totalStockists,
+                        'bonus_payout_cash' => $userData->bonusPayoutForMonth->cash,
+                        'bonus_payout_gold' => $userData->bonusPayoutForMonth->gold,
+                        'bonus_payout_not_chosen' => $userData->bonusPayoutForMonth->bonusNotChosen,
+                        'online_payer' => (bool)$userData->onlinePayer,
+                    ]);
+                    $self->comment("  User Report created id:$userReport->id");
+                }
+            }
+
+            generateReport($this, $year, $month, true, null, $report);
+
+            foreach (Organization::all() as $organization) {
+                generateReport($this, $year, $month, false, $organization->id, $report);
             }
         });
     }
